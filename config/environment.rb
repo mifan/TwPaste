fc6021b49479077baf907fb3ba33480dd8ac78b3
mifan <mifanxp@gmail.com>
2010-02-11 17:30:36 +0800

    replace plugin with gem of acts-as-taggable-on

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,11 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "systemu"
+
   config.gem "uuidtools"
+
+  config.gem "acts-as-taggable-on", :source => "http://gemcutter.org"
+
   # config.gem "rmagick"	
 
   # Only load the plugins named here, in the order given (default is alphabetical).
