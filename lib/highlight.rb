6b2cd2a543989eafbeaacace8837e417d894cc23
mifan <mifanxp@gmail.com>
2010-02-13 14:16:13 +0800

    remove gem dependence of uuidtools and systemu

--- lib/highlight.rb
+++ lib/highlight.rb
@@ -1,30 +1,9 @@
-
-require 'systemu'
-require "uuidtools"
-require "fileutils"
 require 'string'
 require 'coderay'
 
 # snippet highlight
-class Highlight    
+class Highlight
   def self.format(code,type)
-		fname = UUIDTools::UUID.random_create.to_s
-		path = SNIPPET_UPLOAD_TEMP_DIR
-    tmpfile = "#{path}#{fname}"
-		if not File.directory? path
-			FileUtils.makedirs(path)
-		end
-    f = File.new(tmpfile,"a")
-    f.puts code
-    f.close
-    #result = systemu("pygmentize -f html -l #{type} -O encoding=utf8,linenos=1 #{tmpfile}")
-    result = code 
-    File.delete(tmpfile)
-
-    #return result[1].gsub('<div class="highlight">','<div class="inner_code">')
-    
-    result = CodeRay.scan(code,type.to_sym).div
-    result
-
+    CodeRay.scan(code,type.to_sym).div
   end
 end
