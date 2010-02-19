16f245ef6a3d69135674c11c2abcac5d343c2293
mifan <mifanxp@gmail.com>
2010-02-19 19:23:08 +0800

    fix logout problem

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -27,7 +27,7 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default login	_url
+    redirect_back_or_default login_url
   end
 
 end
