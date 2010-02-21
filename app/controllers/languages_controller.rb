fce1cd419c886473bf15821137f72f17c79b156a
mifan <mifanxp@gmail.com>
2010-02-21 17:00:14 +0800

    ommit response of language

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,86 +1,46 @@
 class LanguagesController < ApplicationController
 
-  # GET /languages
-  # GET /languages.xml
   def index
-    @languages = Language.all
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @languages }
-    end
-  end
+    @languages = Language.all(:order => 'name ASC')
+   end
 
-  # GET /languages/1
-  # GET /languages/1.xml
   def show
-    @language = Language.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @language }
-    end
+    @language = Language.find(params[:id])s
   end
 
-  # GET /languages/new
-  # GET /languages/new.xml
   def new
     @language = Language.new
-
-    respond_to do |format|
-      format.html { render :action => "edit" }
-      format.xml  { render :xml => @language }
-    end
   end
 
-  # GET /languages/1/edit
   def edit
     @language = Language.find(params[:id])
   end
 
-  # POST /languages
-  # POST /languages.xml
   def create
     @language = Language.new(params[:language])
-
-    respond_to do |format|
-      if @language.save
-        flash[:notice] = 'Language was successfully created.'
-        format.html { redirect_to(@language) }
-        format.xml  { render :xml => @language, :status => :created, :location => @language }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @language.errors, :status => :unprocessable_entity }
-      end
+    if @language.save
+      flash[:notice] = 'Language was successfully created.'
+      redirect_to(@language)
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /languages/1
-  # PUT /languages/1.xml
+
   def update
     @language = Language.find(params[:id])
-    
-    respond_to do |format|
       if @language.update_attributes(params[:language])
         flash[:notice] = 'Language was successfully updated.'
-        format.html { redirect_to(@language) }
-        format.xml  { head :ok }
+        redirect_to(@language)
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @language.errors, :status => :unprocessable_entity }
+        render :action => "edit"
       end
-    end
   end
 
-  # DELETE /languages/1
-  # DELETE /languages/1.xml
   def destroy
     @language = Language.find(params[:id])
     @language.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(languages_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(languages_url)
   end
 
 end
