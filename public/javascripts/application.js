0809b0a206e3c03a9b65e75bb286e6a6e40d15ba
mifan <mifanxp@gmail.com>
2010-02-22 22:35:44 +0800

    add ajax for toggle of desc and tags

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,32 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {  
+			//click
+			$("#addDescId").click(function(){
+				//$(this).blur();
+				var textDesc = $("textDesc");
+				if(textDesc){
+					textDesc.slideToggle("fast");
+					if(textDesc.visable()){
+						  textDesc.text('+ Hide Description'); 
+					}else{
+						 textDesc.text('+ Add Description'); 
+					}
+				}
+
+				return false;
+			});
+
+			$("#addTagsId").click(function(){
+				//$(this).blur();
+				var textTags = $("textTags");
+				if(textTags){
+					textTags.slideToggle("fast");
+					if(textTags.visable()){
+						  textTags.text('- Hide Tags'); 
+					}else{
+						 textTags.text('+ Add Tags'); 
+					}
+				}
+
+				return false;
+			});
+});
