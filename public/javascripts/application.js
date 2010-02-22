c7d919f5e4bad82c9a8f1e9619f00d840455be97
mifan <mifanxp@gmail.com>
2010-02-22 23:17:47 +0800

    toggle tags and desc again

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,36 +1,34 @@
 $(document).ready(function() {  
-			//click
-			$("#addDescId").click(function(){
-				//$(this).blur();
-				var textDesc = $("#textDesc");
-				if(textDesc){
-					textDesc.slideToggle("fast");
-					if(textDesc.visable()){
-						textDesc.text('+ Hide Description');
-						textDesc.focus();
-					}else{
-						 textDesc.text('+ Add Description');
-						 textDesc.blue();
-					}
-				}
+            //click
+            $("#addDescId").click(function(){
+                //$(this).blur();
+                var textDesc = $("#textDesc");
+                if(textDesc){
+                    textDesc.slideToggle("fast");
+                    if(textDesc.is(":hidden")){
+                         textDesc.text('+ Add Description');
+                         textDesc.blue();
+                    }else{
+                        textDesc.text('+ Hide Description');
+                        textDesc.focus();
+                    }
+                }
+                return false;
+            });
 
-				return false;
-			});
-
-			$("#addTagsId").click(function(){
-				//$(this).blur();
-				var textTags = $("#textTags");
-				if(textTags){
-					textTags.slideToggle("fast");
-					if(textTags.visable()){
-						  textTags.text('- Hide Tags'); 
-						  textTags.focus();
-					}else{
-						 textTags.text('+ Add Tags');
-						 textTags.blur();
-					}
-				}
-
-				return false;
-			});
+            $("#addTagsId").click(function(){
+                //$(this).blur();
+                var textTags = $("#textTags");
+                if(textTags){
+                    textTags.slideToggle("fast");
+                    if(textTags.is(":hidden")){
+                         textTags.text('+ Add Description');
+                         textTags.blue();
+                    }else{
+                        textTags.text('+ Hide Description');
+                        textTags.focus();
+                    }
+                }
+                return false;
+            });
 });
