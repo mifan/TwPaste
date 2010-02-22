1fa47186c25982c692cea8cf70145b8db171fb47
mifan <mifanxp@gmail.com>
2010-02-22 14:04:47 +0800

    add stylesheet generator

---
+++ tools/generate_stylesheets.sh
@@ -0,0 +1,9 @@
+styles='monokai manni perldoc borland colorful default murphy vs trac tango fruity autumn bw emacs pastie friendly native'
+style_file_name='pygments.css'
+echo '' > $style_file_name
+for loop in $styles
+do
+  echo "processing $loop ..."
+  `pygmentize -f html -S #{loop} -a .hl#{loop} >> #{style_file_name}`
+done
+
