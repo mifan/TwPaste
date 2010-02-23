6e935651b9055341ef91f0498e2665c266e9e8b9
mifan <mifanxp@gmail.com>
2010-02-23 10:39:30 +0800

    javascript: toggle problem 1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,34 +1,31 @@
 $(document).ready(function() {  
-            //click
-            $("#addDescId").click(function(){
-                //$(this).blur();
-                var textDesc = $("#textDesc");
-                if(textDesc){
-                    textDesc.slideToggle("fast");
-                    if(textDesc.is(":hidden")){
-                         textDesc.text('+ Add Description');
-                         textDesc.blue();
-                    }else{
-                        textDesc.text('+ Hide Description');
-                        textDesc.focus();
-                    }
-                }
-                return false;
-            });
+  //click
+  $("#addDescId").click(function(){
+    //$(this).blur();
+    var textDesc = $("#textDesc");
+    if(textDesc){
+      textDesc.slideToggle("fast");
+      if(textDesc.is(":hidden")){
+        $("#addDescId").text('+ Add Description');
+      }else{
+        $("#addDescId").text('- Hide Description');
+      }
+    }
+    return false;
+  });
 
-            $("#addTagsId").click(function(){
-                //$(this).blur();
-                var textTags = $("#textTags");
-                if(textTags){
-                    textTags.slideToggle("fast");
-                    if(textTags.is(":hidden")){
-                         textTags.text('+ Add Description');
-                         textTags.blue();
-                    }else{
-                        textTags.text('+ Hide Description');
-                        textTags.focus();
-                    }
-                }
-                return false;
-            });
+  $("#addTagsId").click(function(){
+    //$(this).blur();
+    var textTags = $("#textTags");
+      if(textTags){
+        textTags.slideToggle("fast");
+        if(textTags.is(":hidden")){
+          $("#addTagsId").text('+ Add Tags');
+        }else{
+          $("#addTagsId").text('- Hide Tags');
+        }
+      }
+    return false;
+  });
+
 });
