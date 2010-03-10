8f3a7f5d5764969e77868c390793e110bab9b099
mifan <mifanxp@gmail.com>
2010-03-10 14:17:25 +0800

    add remember me for user session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
         @user = User.find_by_oauth_token(@user.oauth_token)
         unless @user.nil?
 	  @user.update_twitter_info
-          UserSession.create(@user)
+          UserSession.create(@user,true)
           flash.now[:message] = "Welcome back!"
           redirect_back_or_default root_path
         else
