43ea0c629e04b476a382d59f4fba4ea5bd40d5a3
mifan <mifanxp@gmail.com>
2010-03-12 13:23:56 +0800

    comment again 2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
       paste = Paste.find_by_id(params[:paste_id])
-      @comment = paste.comments.build(:user => current_user,:comment => params[:comment])
+      @comment = paste.comments.build(params[:comment])
       @comment.save
       respond_to do |format|
           render :partial => 'pastes/comment' if request.xhr?
