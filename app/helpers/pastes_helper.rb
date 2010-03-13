2c858d446567aabd9fd1c2133c45e2f8b85695ee
mifan <mifanxp@gmail.com>
2010-03-13 22:29:50 +0800

    beta 1.1.1

--- app/helpers/pastes_helper.rb
+++ app/helpers/pastes_helper.rb
@@ -8,4 +8,9 @@ module PastesHelper
     end
     return false
   end
+
+  def paste_title(paste)
+       title = paste.title.blank? 'View' : paste.title 
+       truncate(h(title),:length => 50)
+  end
 end
