3061eb4fadc06e161af709dde7139df6e3a995d9
mifan <mifanxp@gmail.com>
2010-03-14 13:53:36 +0800

    css 5

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     current_user_session.destroy
-    redirect_back_or_default login_url
+    redirect_back_or_default root_url
   end
 
 end
