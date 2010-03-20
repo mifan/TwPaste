f344280bb37d46d913cf9ce8c03bbdec87105249
mifan <mifanxp@gmail.com>
2010-03-20 19:23:50 +0800

    counter bug 1

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,9 +1,9 @@
 class Language < ActiveRecord::Base
   has_many :pastes
-  before_save :update_paste_counter
-  def update_paste_counter
-    #self.pastes_count = self.pastes.count
-  end
+  #before_save :update_paste_counter
+  #def update_paste_counter
+  #  self.pastes_count = self.pastes.count
+  #end
 
   named_scope :top_languages, :limit => 10, :order => 'pastes_count desc'
 
