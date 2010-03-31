7931b0e9de303a4410b386241ebf5135280130f6
mifan <mifanxp@gmail.com>
2010-03-31 13:36:52 +0800

    rm tempfile using Pipe now

--- lib/highlight.rb
+++ lib/highlight.rb
@@ -1,42 +1,49 @@
-require 'tempfile'
-
+#require 'tempfile'
 # paste highlight
 class Highlight    
   def self.format(code,type)
     result = ''
-    tmpfile = nil
-    begin
-      tmpfile = Tempfile.new('twpaste')
-      tmpfile.puts(code)
-      tmpfile.flush
-      result = `pygmentize -f html -l #{type} -O encoding=utf8,linenos=1 #{tmpfile.path}`
-      if $? != 0
-        result = code
-      else
-        result.gsub!('class="highlight"','class="highlight hldefault"')
-      end
-    ensure
-      tmpfile.close if tmpfile
+    #tmpfile = nil
+    #begin
+      #tmpfile = Tempfile.new('twpaste')
+      #tmpfile.puts(code)
+      #tmpfile.flush
+      #result = `pygmentize -f html -l #{type} -O encoding=utf8,linenos=1 #{tmpfile.path}`
+      #if $? != 0
+      # result = code
+      #else
+      #  result.gsub!('class="highlight"','class="highlight hldefault"')
+      #end
+    #ensure
+    #  tmpfile.close if tmpfile
+    #end
+
+    IO.popen("pygmentize -f html -l #{type} -O encoding=utf8,linenos=1", 'r+') do |pipe|
+      pipe.print code
+      pipe.close_write
+      result = pipe.read
+    end
+    if $? != 0
+      result = code
+    else
+      result.gsub!('class="highlight"','class="highlight hldefault"')
     end
     result
   end
 
   def self.format_without_linenos(code,type)
     result = ''
-    tmpfile = nil
-    begin
-      tmpfile = Tempfile.new('twpaste')
-      tmpfile.puts(code)
-      tmpfile.flush
-      result = `pygmentize -f html -l #{type} -O encoding=utf8 #{tmpfile.path}`
-      if $? != 0
-        result = code
-      else
-        result.gsub!('class="highlight"','class="highlight hldefault"')
-      end
-    ensure
-      tmpfile.close if tmpfile
+    IO.popen("pygmentize -f html -l #{type} -O encoding=utf8", 'r+') do |pipe|
+      pipe.print code
+      pipe.close_write
+      result = pipe.read
+    end
+    if $? != 0
+      result = code
+    else
+      result.gsub!('class="highlight"','class="highlight hldefault"')
     end
     result
   end
+
 end
